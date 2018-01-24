160f7802c075e49a0f6f36fb8cae070d77d51e6d
Kyle Hughes <kyle.hughes025@gmail.com>
2018-01-24 15:36:07 -0500

    Setup initial connections to mongoDB and perform some demo operations

--- src/main/java/edu/pennstate/science_olympiad/controllers/FirstController.java
+++ src/main/java/edu/pennstate/science_olympiad/controllers/FirstController.java
@@ -2,11 +2,17 @@ package edu.pennstate.science_olympiad.controllers;
 import edu.pennstate.science_olympiad.people.AUser;
 import edu.pennstate.science_olympiad.people.Admin;
 import edu.pennstate.science_olympiad.people.UserFactory;
+import edu.pennstate.science_olympiad.repository.UserRepository;
+import edu.pennstate.science_olympiad.util.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Set up all the end points, RestController automatically add
@@ -17,6 +23,35 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  */
 @RestController
 public class FirstController {
+    //Log log = LogFactory.getLog(getClass());
+    @Autowired
+    UserRepository userRepository;
+
+    public UserRepository getUserRepository(){
+        return userRepository;
+    }
+
+    public void setUserRepository(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value="createTestUser",method= RequestMethod.GET ,produces={MediaType.APPLICATION_JSON_VALUE})
+    public Object addUser() {
+       // log.info(">>addUser()");
+        AUser user = UserFactory.getInstance().createUser("admin");
+        user.setFirstName("Kyle");
+        user.setLastName("h");
+        user.setEmailAddress("test@email");
+        userRepository.addUser(user);
+
+        AUser coach = UserFactory.getInstance().createUser("coach");
+        coach.setFirstName("coach1");
+        coach.setLastName("co");
+        userRepository.addUser(coach);
+       // log.info("<<addUser()");
+        Pair response = new Pair("success","true");
+        return response;
+    }
 
     @CrossOrigin(origins = "*")
     @RequestMapping(value="users",method= RequestMethod.GET ,produces={MediaType.APPLICATION_JSON_VALUE})
--- src/main/java/edu/pennstate/science_olympiad/people/Admin.java
+++ src/main/java/edu/pennstate/science_olympiad/people/Admin.java
@@ -1,10 +1,14 @@
 package edu.pennstate.science_olympiad.people;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * The admin is the person who creates all of the  {@link edu.pennstate.science_olympiad.Event}s and registers the
  * {@link edu.pennstate.science_olympiad.people.Judge}s and {@link edu.pennstate.science_olympiad.people.Coach}s. This
  * person is the base of everything happening.
  */
+
+
 public class Admin extends AUser {
     private String siteName;
 
